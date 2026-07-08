927526b0b6c6735caafc766e027e04ec6bb5012d
atharvanshtiwari <atharvtiwari999@gmail.com>
2026-07-08 19:39:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -115,10 +115,16 @@ if st.button("🔮 Predict Best Crop", use_container_width=True):
                 """
                 
                 # Fetch response from AI using the client format
-                response = client.models.generate_content(
+                try:
+                    response = client.models.generate_content(
                     model='gemini-2.5-flash',
-                    contents=prompt
-                )
+                    contents=promp
+                    )    
+                except Exception as e:
+                    if "429" in str(e):
+                        st.warning("The AI is currently busy due to high traffic. Please wait a moment and try again!")
+                    else:
+                        st.error("An error occurred. Please try again later.")
                 
                 st.info(response.text)
                 
